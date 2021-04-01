29009c82df4dc9f92dcb84e03da400cbe307a659
Jess-G95 <79566501+Jess-G95@users.noreply.github.com>
2021-04-01 09:00:29 +0100

    Add files via upload

--- x_and_o.py
+++ x_and_o.py
@@ -71,13 +71,14 @@ def player_choice(board):
     position = 0
     
     while position not in [1,2,3,4,5,6,7,8,9] or not space_check(board, position):
-
-        position = int(input('Choose a position (1-9): '))
-        if position not in [1,2,3,4,5,6,7,8,9]:
-            print('Sorry, please enter a number between 1 and 9: ') # add case for ValueError: invalid literal for int() with base 10: '' here and throughout to allow for double press of enter
-        
-        if position != space_check(board,position):
-                print('Oops, that space is already taken. Please choose another number.')
+        try:
+            position = int(input('Choose a position (1-9): '))
+            if position != space_check(board,position):
+                    print('Oops, that space is already taken. Please choose another number.')
+        except ValueError:
+            print("Please enter a number.")
+        except IndexError:
+            print("Please enter a number between 1 and 9.")
     
     return position
 
@@ -112,10 +113,10 @@ while True:
         if ready != 'y' and ready != 'n':
             print('Sorry, please enter either y or n ')
 
-        if ready == 'y':
-            game_on = True
-        else:
-            game_on = False
+    if ready == 'y':
+        game_on = True
+    else:
+        game_on = False
     
     
         # game play
